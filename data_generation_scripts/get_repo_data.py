d9aa28b2bc927e532a5eeabb25866b328341316e
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2022-08-29 09:19:02 -0500

    not sure if I pushed this up or not

--- data_generation_scripts/get_repo_data.py
+++ data_generation_scripts/get_repo_data.py
@@ -85,7 +85,7 @@ def get_dh_repos_data(output_path, rates_df):
         return repo_df
 
 def get_api_data(query):
-    # Thanks https://stackoverflow.com/questions/33878019/how-to-get-data-from-all-pages-in-github-api-with-python 
+    # Thanks https://stackoverflow.com/questions/33878019/how-to-get-data-from-all-pages-in-github-api-with-python
 
     try:
         response = requests.get(f"{query}", headers=auth_headers)
@@ -144,11 +144,11 @@ def get_contributors(repo_df, output_path, ):
     return contributors_df
 
 def get_repo_contributors(repo_df, output_path, rates_df):
-    calls_remaining = rates_df['resources.core.remaining']
+    calls_remaining = rates_df['resources.core.remaining'].values[0]
     while len(repo_df[repo_df.contributors_url.notna()]) > calls_remaining:
         time.sleep(3700)
         rates_df = check_rate_limit()
-        calls_remaining = rates_df['resources.core.remaining']
+        calls_remaining = rates_df['resources.core.remaining'].values[0]
     else:
         if os.path.exists(output_path):
             contributors_df = pd.read_csv(output_path)
@@ -168,24 +168,15 @@ def get_total_commits(url):
 
 def get_commits(repo_df, output_path):
     commits_rows = []
-    counter = 0
     for _, row in tqdm(repo_df.iterrows(), total=repo_df.shape[0], desc="Getting Commits"):
         try:
             url = row.commits_url.split('{')[0]
-            total_commits = int(get_total_commits(url))
-            counter += 1
-            total_loops = round(total_commits/100)
-            for i in range(1, total_loops+1):
-                query = f'{url}?per_page=100&page={i}'
-                response = requests.get(query, headers=auth_headers)
-                counter += 1
-                response_data = response.json()
-                df = pd.json_normalize(response_data)
-                df['repo_id'] = row.id
-                df['html_url'] = row.html_url
-                df['full_name'] = row.full_name
-                df['query'] = query
-                commits_rows.append(df)
+            response_data = get_api_data(url)
+            df = pd.json_normalize(response_data)
+            df['repo_id'] = row.id
+            df['html_url'] = row.html_url
+            df['full_name'] = row.full_name
+            commits_rows.append(df)
         except:
             print(f"Error on getting commits for {row.full_name}")
             continue
@@ -200,30 +191,33 @@ def get_repos_commits(repo_df, output_path, rates_df):
         rates_df = check_rate_limit()
         calls_remaining = rates_df['resources.core.remaining'].values[0]
     else:
-        print(output_path)
         if os.path.exists(output_path):
-            commits_df = pd.read_csv(output_path)
+            commits_df = pd.read_csv(output_path, low_memory=False)
             repos = repo_df.html_url.unique().tolist()
             existing_repos = commits_df[commits_df.html_url.isin(repos)].html_url.unique().tolist()
             if len(existing_repos) != len(repos):
                 missing_commits_repos = set(repos) - set(existing_repos)
                 missing_repos_df = repo_df[repo_df.html_url.isin(missing_commits_repos)]
                 missing_repos_df = get_commits(missing_repos_df, output_path)
-                commits_df = pd.concat([commits_df, missing_repos_df])
-                commits_df = commits_df.reset_index(drop=True)
-                commits_df['commit.committer.date_time'] = pd.to_datetime(commits_df['commit.committer.date'], format='%Y-%m-%dT%H:%M:%SZ')
-                commits_df['date'] = commits_df['commit.committer.date_time'].dt.date
-                commits_df['datetime'] = pd.to_datetime(commits_df['date'])
-                commits_df.to_csv(output_path, index=False)
+                print(missing_repos_df)
+                print(commits_df)
+                final_commits_df = pd.concat([commits_df, missing_repos_df])
+                final_commits_df = final_commits_df.reset_index(drop=True)
+                final_commits_df['commit.committer.date_time'] = pd.to_datetime(final_commits_df['commit.committer.date'], format='%Y-%m-%dT%H:%M:%SZ')
+                final_commits_df['date'] = final_commits_df['commit.committer.date_time'].dt.date
+                final_commits_df['datetime'] = pd.to_datetime(final_commits_df['date'])
+                final_commits_df.to_csv(output_path, index=False)
+            else:
+                final_commits_df = commits_df
         else:
             # tqdm.pandas(desc="Getting Commits")
             # repo_df['commits'] = repo_df.progress_apply(get_commits, axis=1, output_path=output_path)
             # repo_df.to_csv(output_path, index=False)
-            commits_df = get_commits(repo_df, output_path)
-    return commits_df
+            final_commits_df = get_commits(repo_df, output_path)
+    return final_commits_df
 
 if __name__ == "__main__":
-    repo_df = get_all_repos_topic_dh('../data/repos_topic_dh.csv')
+    repo_df = get_dh_repos_data('../data/repos_topic_dh.csv')
     repo_languages_df = get_repo_languages(repo_df, '../data/repos_topic_dh_languages.csv')
     contributors_df = get_repo_contributors(repo_df, '../data/repos_topic_dh_contributors.csv')
     commits_df = get_repos_commits(repo_df, '../data/repos_topic_dh_commits.csv')
