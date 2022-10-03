8de011a1763df3511d073ab1b52431ede0298378
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2022-10-03 09:35:45 -0500

    updates

--- data_generation_scripts/generate_forks_join_tables.py
+++ data_generation_scripts/generate_forks_join_tables.py
@@ -2,3 +2,18 @@
 # forks
 # PRs
 # issues
+
+import time
+import pandas as pd
+import requests
+import os
+from tqdm import tqdm
+import apikey
+import sys
+sys.path.append("..")
+from data_generation_scripts.utils import *
+
+
+auth_token = apikey.load("DH_GITHUB_DATA_PERSONAL_TOKEN")
+
+auth_headers = {'Authorization': f'token {auth_token}','User-Agent': 'request'}
