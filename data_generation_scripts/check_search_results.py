17eca5d14afb31634a4d4a5a145facbc116ab295
ZoeLeBlanc <zgleblanc@gmail.com>
2022-12-15 08:34:54 -0600

    finished cleaning repos but realize I didn't include multilingual as an option 😭 so might need to rethink some things

--- data_generation_scripts/check_search_results.py
+++ data_generation_scripts/check_search_results.py
@@ -15,26 +15,34 @@ user_join_output_path = "../data/derived_files/search_queries_user_join_subset_d
 search_queries_repo_df = pd.read_csv(repo_join_output_path)
 search_queries_user_df = pd.read_csv(user_join_output_path)
 
-# repo_join_output_path = "../data/derived_files/updated_search_queries_repo_join_subset_dh_dataset.csv"
 
 needs_checking = search_queries_repo_df[(search_queries_repo_df['keep_repo'] == True) & (search_queries_repo_df['finalized_language'].isna())]
 needs_checking.loc[needs_checking.detected_language.isna(), 'detected_language'] = None
 needs_checking = needs_checking.reset_index(drop=True)
 for index, row in needs_checking.iterrows():
+    all_rows = search_queries_repo_df[(search_queries_repo_df['full_name'] == row.full_name)]
     print(f"On {index} out of {len(needs_checking)}")
     print(f"This repo {row.full_name} ")
     print(f"Repo URL: {row.html_url}")
     print(f"Repo Description: {row.description}")
-    print(f"Repo Natural Language: {row.natural_language}")
-    print(f"Repo Detected Language: {row.detected_language}")
-    print(f"Repo Search Query: {row.search_query}")
-    print(f"Repo Search Query Term: {row.search_term}")
-    print(f"Repo Search Query Source Term: {row.search_term_source}")
+    print(f"Repo Natural Language: {all_rows.natural_language.unique()}")
+    print(f"Repo Detected Language: {all_rows.detected_language.unique()}")
+    print(f"Repo Search Query: {all_rows.search_query.unique()}")
+    print(f"Repo Search Query Term: {all_rows.search_term.unique()}")
+    print(f"Repo Search Query Source Term: {all_rows.search_term_source.unique()}")
     # Input answer
     answer = console.input("stay in the dataset? (y/n)")
     if answer == 'n':
         row.keep_repo = False
-    potential_language = row.detected_language if row.detected_language != None else row.natural_language
+
+    detected_languages = all_rows[all_rows.detected_language.notna()].detected_language.unique().tolist()
+    natural_languages = all_rows[all_rows.natural_language.notna()].natural_language.unique().tolist()
+
+    detected_languages = row.detected_language if len(detected_languages) == 1 else str(detected_languages).replace('[', '').replace(']', '')
+    natural_languages = row.natural_language if len(natural_languages) == 1 else str(natural_languages).replace('[', '').replace(']', '')
+
+    potential_language = detected_languages if len(detected_languages) != 0 else natural_languages
+    potential_language = potential_language if len(potential_language) != 0 else 'None'
 
     if ',' in potential_language:
         if 'fr' in potential_language:
@@ -51,7 +59,7 @@ for index, row in needs_checking.iterrows():
     if language_answers == 'n':
         final_language = console.input("What is the correct language? ")
         row.finalized_language = final_language
-    search_queries_repo_df.loc[(search_queries_repo_df.full_name == row.full_name) & (search_queries_repo_df.search_query == row.search_query), 'keep_repo'] = row.keep_repo
-    search_queries_repo_df.loc[(search_queries_repo_df.full_name == row.full_name) & (search_queries_repo_df.search_query == row.search_query), 'finalized_language'] = row.finalized_language
+    search_queries_repo_df.loc[(search_queries_repo_df.full_name == row.full_name), 'keep_repo'] = row.keep_repo
+    search_queries_repo_df.loc[(search_queries_repo_df.full_name == row.full_name), 'finalized_language'] = row.finalized_language
     search_queries_repo_df.to_csv(repo_join_output_path, index=False)
     print(u'\u2500' * 10)
